86f1d1377c3bbaca290eb897746354482438b3f7
yevhen198821-cmyk <yevhen198821@gmail.com>
2026-08-09 18:30:51 +0200

    feat(p4a): expose timeline web storage contracts

---
+++ packages/timeline-web/src/index.ts
@@ -0,0 +1,21 @@
+export {
+  TIMELINE_BOOTSTRAP_VERSION,
+  TIMELINE_INDEXEDDB_DATABASE_NAME,
+  TIMELINE_INDEXEDDB_EVENT_INDEXES,
+  TIMELINE_INDEXEDDB_STORES,
+  TIMELINE_INDEXEDDB_VERSION,
+  TIMELINE_SEED_VERSION,
+  TIMELINE_STORAGE_SCHEMA_VERSION,
+  timelineIndexedDbSchemaV1,
+  type IndexedDbTimelineEventRecord,
+  type IndexedDbTimelineQuarantineRecord,
+  type TimelineBootstrapMetadata,
+  type TimelineIndexedDbSchemaDefinition,
+  type TimelineStorageQuarantineReason,
+  type TimelineStorageSchemaVersion,
+} from './persistence/indexeddb/timeline-indexeddb-schema';
+export {
+  isTimelineBootstrapMetadata,
+  validateIndexedDbTimelineEventRecord,
+  type TimelineIndexedDbRecordValidationResult,
+} from './persistence/indexeddb/timeline-indexeddb-validation';
